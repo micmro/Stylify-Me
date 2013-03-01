24776a0a7ce1ea5e3c8371996c36e4c8064b1085
Michael Mrowetz <me@michaelmrowetz.com>
2013-03-01 00:04:36 -0500

    wip

--- routes/phantom.js
+++ routes/phantom.js
@@ -21,12 +21,13 @@ function isValidURL(url){
 exports.query = function(req, res){
 	var url = req.query["url"];
 	if(url && isValidURL(url)){
+		var filePath = path.join(__dirname, '../phantom_files/color-crawler.js');
 	  	var childArgs = [
-		  path.join(__dirname, 'phantom_files/color-crawler.js'),
+		  filePath,
 		  req.query["url"]
 		];
 	  	childProcess.execFile(binPath, childArgs, function(err, stdout, stderr) {
-	  		res.send('PHANTOM SAYS:' + stdout);
+	  		res.send('PHANTOM SAYS('+url+'):' + filePath + ' ' + stdout);
 		});
   	}else{
   		res.send('INVALID URL!');
