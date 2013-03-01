956bee15b4ee8c57e0a3696078844e5ff65166cf
Michael Mrowetz <me@michaelmrowetz.com>
2013-02-28 21:15:05 -0500

    revert

--- web.js
+++ web.js
@@ -1,24 +1,10 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-var childProcess = require('child_process')
-var phantomjs = require('phantomjs')
-var binPath = phantomjs.path
+
 
 app.get('/', function(request, response) {
-  	
-
-
-
-	var childArgs = [
-	  path.join(__dirname, 'color-crawler.js'),
-	  'http://google.com'
-	]
-
-	childProcess.execFile(binPath, childArgs, function(err, stdout, stderr) {
-	  // handle results
-	  response.send('Hello World!' + stdout);
-	})
+  	response.send('Hello World!')
 });
 
 var port = process.env.PORT || 5000;
