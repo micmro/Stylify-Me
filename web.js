b66f510f83c9dd2d48cbfbfa9589c2a85408ab87
Michael Mrowetz <me@michaelmrowetz.com>
2013-02-28 22:36:37 -0500

    xx

--- web.js
+++ web.js
@@ -1,8 +1,8 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-var childProcess = require('child_process')
-var binPath = "vendor/phantomjs/bin/phantomjs"//LD_LIBRARY_PATH
+//var childProcess = require('child_process')
+//var binPath = "vendor/phantomjs/bin/phantomjs"//LD_LIBRARY_PATH
 
 
 
@@ -16,7 +16,7 @@ app.get('/', function(request, response) {
 app.get('/testA', function(request, response) {
 	response.send('Hi there')
 }
-
+/*
 app.get('/testB', function(request, response) {
   	var childArgs = [
 	  path.join(__dirname, 'color-crawler.js'),
@@ -36,7 +36,7 @@ app.get('/testC', function(request, response) {
   		response.send(stdout)
 	})
 });
-
+*/
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
