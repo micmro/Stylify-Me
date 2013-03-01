219a4dab725caba39f478f699d2d24d8d6d62899
Michael Mrowetz <me@michaelmrowetz.com>
2013-03-01 00:38:49 -0500

    wip

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ var express = require('express')
   , routes = require('./routes')
   , phantom = require('./routes/phantom')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , childProcess = require('child_process')
+  , binPath = "vendor/phantomjs/bin/phantomjs";
 
 var app = express();
 
@@ -27,8 +29,33 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
+
+function isValidURL(url){
+    var urlRegEx = /(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+
+    if(urlRegEx.test(url)){
+        return true;
+    }else{
+        return false;
+    }
+} 
+
 app.get('/', routes.index);
-app.get('/query', phantom.query);
+app.get('/query', function(req, res){
+  var url = req.query["url"];
+  if(url && isValidURL(url)){
+    var filePath = path.join(__dirname, 'color-crawler.js');
+      var childArgs = [
+      filePath,
+      req.query["url"]
+    ];
+      childProcess.execFile(binPath, childArgs, function(err, stdout, stderr) {
+        res.send('PHANTOM SAYS('+url+'):'  + '\n' + __dirname + '\n' + '\n' + filePath + '\n' + err + '\n' + stdout + '\n' + stderr);
+    });
+    }else{
+      res.send('INVALID URL!');
+    }
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
