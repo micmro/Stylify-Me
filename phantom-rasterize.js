25df418e186d50aca09aaca88d4d51a6de5a7348
Michael Mrowetz <me@michaelmrowetz.com>
2013-04-12 00:31:15 -0400

    fix rasterize

--- phantom-rasterize.js
+++ phantom-rasterize.js
@@ -1,14 +1,65 @@
 var page = require('webpage').create(),
     system = require('system'),
-    address, output, size;
+    args = require('system').args,
+    address, isDebug, output, size;
+
+/*phantom settings*/
+phantom.cookiesEnabled = false;
+
+
+//print out console logs on page level
+page.onConsoleMessage = function (msg) { 
+    if (msg.indexOf("Unsafe JavaScript attempt to access frame with URL") > -1){
+        return; 
+    }
+    if(isDebug){
+        console.log('CONSOLE: ' + msg);
+    }
+};
+
+page.onAlert = function (msg) {
+    //ignore alerts
+    //console.log('ALERT: ' + msg);
+};
+
+//error tracing
+page.onError = function(msg, trace) {
+    var msgStack = ['ERROR: ' + msg];
+    if (trace) {
+        msgStack.push('TRACE:');
+        trace.forEach(function(t) {
+            msgStack.push(' -> ' + (t.file||t.sourceURL) + ': ' + t.line + (t.function ? ' (in function ' + t.function + ')' : ''));
+        });
+    }
+    console.error(msgStack.join('\n'));
+    phantom.exit();
+    return;
+};
+
+phantom.onError = function(msg, trace) {
+    var msgStack = ['PHANTOM ERROR: ' + msg];
+    if (trace) {
+        msgStack.push('TRACE:');
+        trace.forEach(function(t) {
+            msgStack.push(' -> ' + (t.file||t.sourceURL) + ': ' + t.line + (t.function ? ' (in function ' + t.function + ')' : ''));
+        });
+    }
+    console.error(msgStack.join('\n'));
+    phantom.exit();
+};
+
+
 
 if (system.args.length < 3 || system.args.length > 5) {
     console.log('Usage: rasterize.js URL filename [paperwidth*paperheight|paperformat] [zoom]');
     console.log(' paper (pdf output) examples: "5in*7.5in", "10cm*20cm", "A4", "Letter"');
     phantom.exit(1);
 } else {
-    address = system.args[1];
-    output = system.args[2];
+    address = args[1];
+    output = args[2];
+    isDebug = args[5] === "true";
+    /*render settings*/
+    page.zoomFactor = 1;
     page.viewportSize = { width: 600, height: 600 };
     if (system.args.length > 3 && system.args[2].substr(-4) === ".pdf") {
         size = system.args[3].split('*');
@@ -23,11 +74,10 @@ if (system.args.length < 3 || system.args.length > 5) {
             console.log('Unable to load the address!');
             phantom.exit();
         } else {
-         //   window.setTimeout(function () {
+            window.setTimeout(function () {
                 page.render(output);
-                console.log("done");
                 phantom.exit();
-          //  }, 200);
+            }, 200);
         }
     });
 }
