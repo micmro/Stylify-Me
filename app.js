2d394e5d64a1b08186a7f017ace05e1268ec261e
Michael <michaelmrowetz@gmail.com>
2019-04-07 23:40:49 +0200

    chore: update to ES6 syntax

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 /* Module dependencies.*/
-var http = require('http')
+const http = require('http')
 	, path = require('path')
 	, fs = require('fs')
 	, childProcess = require('child_process')
@@ -14,14 +14,14 @@ var http = require('http')
 	, phantomjs = require('phantomjs-prebuilt');
 
 /* Variables / Config */
-var config = {
+const config = {
 	binPath: phantomjs.path
 	, crawlerFilePath: "stylify-crawler.js"
 	, rasterizeFilePath: "phantom-rasterize.js"
 	, screenshotCacheTime: 5000 * 1 //in ms (1000ms = 1 sec)
 };
 
-var app = express();
+const app = express();
 
 app.set('port', process.env.PORT || 5000);
 app.use(compression());
@@ -42,19 +42,19 @@ if (app.get('env') === 'production') {
 	app.use(errorhandler());
 }
 
-app.use(function (err, req, res, next) {
+app.use((err, req, res, next) => {
 	console.error(err.stack);
 	res.send(500, '<h1>Something\'s gone wrong!</h1><p>Please try to refresh the page</p>');
 });
 
 
-var utils = {
-	isValidURL: function (url) {
-		return (/[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,18}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi).test(url);
+const utils = {
+	isValidURL: (urlPath) => {
+		return (/[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,18}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi).test(urlPath);
 	},
-	deleteFile: function (filePath) {
+	deleteFile: (filePath) => {
 		try {
-			fs.unlink(filePath, function () {
+			fs.unlink(filePath, () => {
 				console.log("file deleted", filePath, arguments);
 			});
 		} catch (e) {
@@ -62,14 +62,14 @@ var utils = {
 		}
 	},
 
-	isRefererValid: function (referer) {
-		var validRefs = ["http://stylifyme.com",
+	isRefererValid: (referer) => {
+		const validRefs = ["http://stylifyme.com",
 						"http://www.stylifyme.com",
 						"http://stylify.herokuapp.com",
 						"http://localhost:9185",
 						"http://localhost:7210",
-						"http://localhost:" + app.get('port')]
-			, isvalid = false;
+						"http://localhost:" + app.get('port')];
+		let isvalid = false;
 		for (valRef in validRefs) {
 			if (referer.indexOf(validRefs[valRef]) == 0) {
 				isvalid = true;
@@ -82,8 +82,8 @@ var utils = {
 		return isvalid;
 	},
 
-	parsePhantomResponse: function (err, stdout, stderr, onsuccess, onerror) {
-		var jsonResponse = {};
+	parsePhantomResponse: (err, stdout, stderr, onsuccess, onerror) => {
+		let jsonResponse = {};
 		try {
 			if (err || stderr) {
 				console.log("ERR:PHANTOM>" + (stderr || err));
@@ -93,7 +93,7 @@ var utils = {
 				console.log("ERR:PHANTOM>" + stdout);
 
 
-				var errorCode = stdout.match(/ERROR\((\d+)\)/)[1];
+				const errorCode = stdout.match(/ERROR\((\d+)\)/)[1];
 				switch (errorCode) {
 					case "404": onerror("Fail to load the current url - please make sure you don't have typos", errorCode);
 						break;
@@ -120,29 +120,27 @@ var utils = {
 			onerror("Fail to parse response", "999");
 		}
 	},
-	makeFilename: function (url) {
+	makeFilename: (url) => {
 		return url.replace(/http:\/\//, "").replace(/[\/:/]/g, "_");
 	}
 }
 
 
 /* Routes */
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
 	res.redirect(301, "http://stylifyme.com/");
 });
 
-app.get('/about', function (req, res) {
+app.get('/about', (req, res) => {
 	res.redirect(301, "http://stylifyme.com/about-us");
 });
 
 //renders html for PDF
-app.get('/renderpdfview', function (req, res) {
-	var referer = req.get("Referer") || "http://stylify.herokuapp.com"
-		, jsonResponse = {}
+app.get('/renderpdfview', (req, res) => {
+	const referer = req.get("Referer") || "http://stylify.herokuapp.com"
 		, showImage = true
-		, debugMode = false
-		, test = "durp"
-		, url, childArgs, phantomProcess;
+		, debugMode = false;
+	let url, childArgs, phantomProcess;
 	if (utils.isRefererValid(referer)) {
 		url = req.query["url"];
 		if (url && utils.isValidURL(url)) {
@@ -156,11 +154,11 @@ app.get('/renderpdfview', function (req, res) {
 				, `--debug=${debugMode}`
 			];
 			try {
-				phantomProcess = childProcess.execFile(config.binPath, childArgs, { timeout: 25000 }, function (err, stdout, stderr) {
-					utils.parsePhantomResponse(err, stdout, stderr, function (jsonResponse) {
+				phantomProcess = childProcess.execFile(config.binPath, childArgs, { timeout: 25000 }, (err, stdout, stderr) => {
+					utils.parsePhantomResponse(err, stdout, stderr, (jsonResponse) => {
 						res.render('pdfbase', { title: 'Stylify Me - Extract', pageUrl: url, data: jsonResponse });
 					}
-						, function (errorMsg, errorCode) {
+						, (errorMsg, errorCode) => {
 							phantomProcess.kill();
 							res.status(503).jsonp({ "error": errorMsg, "errorCode": errorCode || "000" });
 						});
@@ -180,18 +178,18 @@ app.get('/renderpdfview', function (req, res) {
 });
 
 //returns PDF file
-app.get('/getpdf', function (req, res) {
-	var referer = req.get("Referer") || "http://stylify.herokuapp.com"
-		, url, childArgs, filename, phantomProcess;
+app.get('/getpdf', (req, res) => {
+	const referer = req.get("Referer") || "http://stylify.herokuapp.com";
+	let url, childArgs, filename, phantomProcess;
 	if (utils.isRefererValid(referer)) {
 		url = req.query["url"];
 		if (url && utils.isValidURL(url)) {
 			filename = "public/pdf/temp" + utils.makeFilename(url) + "_" + new Date().getTime().toString() + ".pdf";
 			childArgs = [config.rasterizeFilePath, req.protocol + "://" + req.get('host') + "/renderpdfview?url=" + encodeURIComponent(url), filename, "A4"];
 			try {
-				phantomProcess = childProcess.execFile(config.binPath, childArgs, { timeout: 50000 }, function (err, stdout, stderr) {
+				phantomProcess = childProcess.execFile(config.binPath, childArgs, { timeout: 50000 }, (err, stdout, stderr) => {
 					console.log("LOG: CREATED PDF", filename);
-					res.download(filename, "stylify-me " + utils.makeFilename(url) + ".pdf", function (err) {
+					res.download(filename, "stylify-me " + utils.makeFilename(url) + ".pdf", (err) => {
 						utils.deleteFile(filename);
 						phantomProcess.kill();
 					});
@@ -212,21 +210,20 @@ app.get('/getpdf', function (req, res) {
 
 
 //returns stylify json
-app.get('/query', function (req, res) {
-	var referer = req.get("Referer") || "http://stylify.herokuapp.com"
-		, jsonResponse = {}
+app.get('/query', (req, res) => {
+	const referer = req.get("Referer") || "http://stylify.herokuapp.com"
 		, showImage = true
-		, debugMode = false
-		, url, childArgs, phantomProcess;
+		, debugMode = false;
+	let url, childArgs, phantomProcess;
 	if (utils.isRefererValid(referer)) {
 		url = req.query["url"];
 		if (url && utils.isValidURL(url)) {
 			childArgs = [config.crawlerFilePath, req.query["url"], showImage, debugMode];
 			try {
-				phantomProcess = childProcess.execFile(config.binPath, childArgs, { timeout: 25000 }, function (err, stdout, stderr) {
-					utils.parsePhantomResponse(err, stdout, stderr, function (jsonResponse) {
+				phantomProcess = childProcess.execFile(config.binPath, childArgs, { timeout: 25000 }, (err, stdout, stderr) => {
+					utils.parsePhantomResponse(err, stdout, stderr, (jsonResponse) => {
 						res.status(200).jsonp(jsonResponse);
-					}, function (errorMsg, errorCode) {
+					}, (errorMsg, errorCode) => {
 						phantomProcess.kill();
 						res.status(200).jsonp({ "error": errorMsg, "errorCode": errorCode || "000" });
 					});
@@ -247,10 +244,11 @@ app.get('/query', function (req, res) {
 
 
 //returns phantom js version number
-app.get('/version', function (req, res) {
-	var childArgs = ["--version"], phantomProcess;
+app.get('/version', (req, res) => {
+	const childArgs = ["--version"];
+	let phantomProcess;
 	try {
-		phantomProcess = childProcess.execFile(config.binPath, childArgs, { timeout: 5000 }, function (err, stdout, stderr) {
+		phantomProcess = childProcess.execFile(config.binPath, childArgs, { timeout: 5000 }, (err, stdout, stderr) => {
 			res.status(200).jsonp((err || stdout || stderr).replace(/[\n\r]+/g, ""));
 		});
 	} catch (err) {
@@ -265,6 +263,6 @@ app.get('/version', function (req, res) {
    // res.redirect(301, "http://stylifyme.com");
 });*/
 
-http.createServer(app).listen(app.get('port'), function () {
+http.createServer(app).listen(app.get('port'), () => {
 	console.log("Express server listening on port " + app.get('port'));
 });
