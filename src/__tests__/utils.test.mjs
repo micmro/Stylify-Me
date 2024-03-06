687cefa5063857a4ce4f2ca2d1bf6cb6c676a8ba
Michael <michaelmrowetz@gmail.com>
2024-03-06 17:04:20 +0900

    fix: tests

--- src/__tests__/utils.test.mjs
+++ src/__tests__/utils.test.mjs
@@ -7,10 +7,11 @@ describe("utils", () => {
   });
 
   test.each([
-    ["http://stylifyme.com", "stylifyme.com"],
-    ["http://www.stylifyme.com", "www.stylifyme.com"],
-    ["http://api.stylifyme.com", "api.stylifyme.com"],
-    ["https://google.com", "google.com"],
+    ["http://stylifyme.com", "stylifyme_com"],
+    ["http://www.stylifyme.com", "www_stylifyme_com"],
+    ["http://stylify.herokuapp.com", "stylify_herokuapp_com"],
+    ["http://api.stylifyme.com", "api_stylifyme_com"],
+    ["https://google.com", "google_com"],
     [`http://localhost:4000`, `localhost_4000`],
   ])("accepts %s", (url, result) => {
     expect.assertions(1);
